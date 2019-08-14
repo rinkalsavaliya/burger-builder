751e32a63755ff86aeb71fd70fd4bfb3fcbe2a45
rinkalsavaliya <rinkal@scaletech.xyz>
2019-08-14 12:59:04 +0530

    api key changes

--- src/axios.js
+++ src/axios.js
@@ -10,4 +10,4 @@ instance.defaults.headers.get['xyz'] = 'xyz';
 instance.defaults.headers.post['jkl'] = 'jkl';
 
 export default instance;
-export const apiKey = '';
+export const apiKey = process.env.API_KEY || '';
