b886a970915232b838dac0a79885299c9f7cfc40
rinkalsavaliya <rinkal@scaletech.xyz>
2019-08-14 15:11:32 +0530

    log remove

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const config = {
   apiUrl: process.env.REACT_APP_API_URL
 };
 if (process.env.NODE_ENV === 'production') {
-  // window.console.log = () => { };
+  window.console.log = () => { };
 }
 console.log(process.env, config);
 const apiKey = process.env.REACT_APP_API_KEY;
